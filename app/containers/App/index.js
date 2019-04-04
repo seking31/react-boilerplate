521b6bcca9c5faee172f8b2d7a1c1185f3df632f
dev1 <dev1@C07W40WRG1J2>
2019-04-04 15:00:58 -0600

    login and home pages added

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -10,7 +10,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
+import HomePage from 'containers/HomePage';
+import LogInPage from 'containers/LogInPage';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 import GlobalStyle from '../../global-styles';
@@ -20,6 +21,7 @@ export default function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={LogInPage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
